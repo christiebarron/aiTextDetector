6a3e368c5ca787d389438bbdfe0796a6672e836c
Christie Barron <christie.m.barron@gmail.com>
2023-03-31 18:56:34 -0400

    dataset adding.

--- asapData/scripts/textGeneration.py
+++ asapData/scripts/textGeneration.py
@@ -11,32 +11,28 @@ openai.api_key =  OPENAI_API_KEY #os.getenv("OPENAI_API_KEY")
 
 # see   https://platform.openai.com/docs/api-reference/completions/create
 
-ai_response = []
-
 #model setup
 model = "text-davinci-003" #the id of the model to include
 prompt = "Write a letter to your local newspaper stating your opinion on the effects computers have on people. Persuade the readers to agree with you." #the prompt to provide 
-essay_id = 1
-essay_type = "persuasive/narrative"
+essay_id = 1 #an id of the essay to save to the text file
+essay_type = "persuasive/narrative" #type of essay to potentially the save
 max_tokens = 300 #maximum number of tokens to include
 n = 30 #number of essays to generate in single API call
 
-ai_response.append(openai.Completion.create(model =  model, prompt = prompt, max_tokens = max_tokens, n = n))
-#set up lists to store variables in
-ai_response_text = []
-ai_response_model = []
-ai_response_prompt = []
-ai_response_essay_id = []
-ai_response_essay_type = []
-counter = 0
+count = 0 #provide count number as text id to save
 
-#extract information from the saved data
-for i in range(len(ai_response)): #loop through all API calls
-    for x in range(len(ai_response[i]["choices"])): #loop through all text generated within single API call
-        ai_response_text.append(ai_response[i]["choices"][x]["text"]) #save text response
-        ai_response_model.append(ai_response[i]["model"]) #save the large language model used to generate the text
-        ai_response_prompt.append(prompt) #save the written prompt given.
-        ai_response_essay_id.append(essay_id) #save the essay id
-        ai_response_essay_type.append(essay_type)
+for i in range (7): #loop through i API calls
+
+    result = openai.Completion.create(model =  model, prompt = prompt, max_tokens = max_tokens, n = n) #API call, keeping result
+    
+    
+    for x in range(len(result["choices"])):
+        count = count + 1
+        text = result["choices"][x]["text"]
+        model = result["model"]
+
+        with open(f'../rawData/aiEssays/eid{essay_id}_{model}_{count}.txt', 'w') as f:
+            f.write(text)
+
 
 pd.DataFrame({"ai_prompt" : ai_response_prompt, "ai_llm" : ai_response_model, "ai_text" : ai_response_text}).to_excel("../rawData/aiGenerated.xlsx", sheet_name= "test2")
