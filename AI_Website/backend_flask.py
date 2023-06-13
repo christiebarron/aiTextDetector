f83aa11832425bdb56ed1118e9f62bd2a4755aac
Maisam Alam <maisam.alam@gmail.com>
2023-06-12 21:47:21 -0400

    login page finally works!

--- AI_Website/backend_flask.py
+++ AI_Website/backend_flask.py
@@ -1,68 +1,33 @@
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, UserMixin, login_user, login_required
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://textual_detectives_db_user:twVKnWDlc2A0Ou6FfFdQjCizFVrnxGgN@dpg-chinpee4dad01anku4v0-a/textual_detectives_db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://textual_detectives_db_user:twVKnWDlc2A0Ou6FfFdQjCizFVrnxGgN@dpg-chinpee4dad01anku4v0-a.ohio-postgres.render.com/textual_detectives_db'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-
-    new_user = User(username=data['username'], password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'Registered successfully!'}), 200
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = User.query.filter_by(username=data['username']).first()
+    username = data.get('username')
+    password = data.get('password')
 
-    if not user or not check_password_hash(user.password, data['password']):
-        return jsonify({'message': 'Login Unsuccessful. Please check username and password'}), 401
-
-    login_user(user)
-
-    return jsonify({'message': 'Login Successful!'}), 200
-
-@app.route('/next_page')
-@login_required
-def next_page():
-    return render_template('next_page.html', username=current_user.username)
-
-@app.route('/logout', methods=['POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect('/login')
-
-@app.route('/create_admin', methods=['GET'])
-def create_admin():
-    hashed_password = generate_password_hash('cvpgm2023', method='sha256')
-    new_user = User(username='admin', password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({'message': 'Admin user created successfully'}), 200
+    if username == 'admin' and password == 'cvpmm2023':
+        # Login successful
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        # Login unsuccessful
+        return jsonify({'message': 'Login unsuccessful'}), 401
+    return render_template('login.html')
+@app.route('/analysis_page')
+def analysis_page():
+    return render_template('analysis_page.html')
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    with app.app_context():
+         app.run(host='0.0.0.0', port=5000, debug=True)
